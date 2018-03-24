0352fd80823963658d6a89edf24b30cdfaa4e7ae
brad <bfellows@gmail.com>
2018-03-24 14:27:15 -0700

    add react app

---
+++ src/client/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import RoadCaptainMapComponent from './roadcaptain-map';
+import './index.css';
+
+ReactDOM.render(
+  <RoadCaptainMapComponent />,
+  document.getElementById('root')
+)
---
+++ src/client/src/roadcaptain-map/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { GoogleMap, Marker } from 'react-google-maps'
+//
+// const roadCaptainMapComponent = (props) =>
+//   <GoogleMap
+//     defaultZoom={8}
+//     defaultCenter={{lat: -42,lon: 122}}
+//   >
+//     <Marker position={{ lat: -42, lon: 122 }} />
+//   </GoogleMap>
+
+class RoadCaptainMapComponent extends React.Component {
+  render() {
+    return (
+      <span>werd</span>
+    )
+  }
+}
+
+export default RoadCaptainMapComponent;
