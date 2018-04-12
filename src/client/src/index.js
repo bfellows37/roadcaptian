526db567365239b3d2912eb3aa5c2ae5ebd94e83
brad <bfellows@gmail.com>
2018-04-11 22:42:45 -0700

    refactor into async main

--- src/client/src/index.js
+++ src/client/src/index.js
@@ -3,30 +3,42 @@ import ReactDOM from 'react-dom';
 import { RoadCaptainMap } from './roadcaptain-map';
 import './index.css';
 
-const getTrip = async () => fetch('/api/roadtrip', {
-  body: JSON.stringify({
-    direction: 's',
-    days: '5',
-    distancePerDay: 200,
-    coordinates: [-122.32, 47.83]
-  }), // must match 'Content-Type' header
-  cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-  headers: {
-    'content-type': 'application/json'
-  },
-  method: 'POST'
-});
+const getTrip = async (opts) => {
+  return fetch('/api/roadtrip', {
+    body: JSON.stringify(opts),
+    cache: 'no-cache',
+    headers: {
+      'content-type': 'application/json'
+    },
+    method: 'POST'
+  }).then(response => response.json());
+};
 
-getTrip().then(response => response.json()).then((response) => {
+const main = async () => {
+  try{
+    const trip = await getTrip({
+      direction: 's',
+      days: '5',
+      distancePerDay: 200,
+      coordinates: [-122.32, 47.83]
+    });
+    ReactDOM.render(
+      <RoadCaptainMap
+        position={{lat: trip.roadtrip[0].gps.coordinates[1], lng: trip.roadtrip[0].gps.coordinates[0]}}
+        route={trip.decoded}
+        googleMapURL={'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places'}
+        loadingElement={<div style={{height: `100%`}} />}
+        containerElement={<div style={{height: `720px`}} />}
+        mapElement={<div style={{height: `100%`}} />}
+      />,
+      document.getElementById('root')
+    );
+  } catch (e) {
+    throw new Error('oops');
+  }
+}
+main().catch(e => {
   ReactDOM.render(
-    <RoadCaptainMap
-      position={{lat: response.roadtrip[0].gps.coordinates[1], lng: response.roadtrip[0].gps.coordinates[0]}}
-      route={response.decoded}
-      googleMapURL={'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places'}
-      loadingElement={<div style={{height: `100%`}} />}
-      containerElement={<div style={{height: `720px`}} />}
-      mapElement={<div style={{height: `100%`}} />}
-    />,
-    document.getElementById('root')
+    <div>oops</div>, document.getElementById('root')
   );
 });
